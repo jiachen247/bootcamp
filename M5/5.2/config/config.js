8e2894ca2b3cd2e0a86f4061d5f44e8531ee695f
raivatshah <hiraivat@gmail.com>
2022-03-20 21:08:04 +0530

    add solution for ICE M5.2

---
+++ M5/5.2/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: 'raivatshah',
+    password: null,
+    database: 'ajax_ecom_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
---
+++ M5/5.2/models/index.mjs
@@ -0,0 +1,21 @@
+import pkg from 'sequelize';
+const { Sequelize } = pkg;
+
+import allConfig from '../config/config.js';
+
+import itemModel from './item.mjs';
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = allConfig[env];
+
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.Item = itemModel(sequelize, Sequelize.DataTypes);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
