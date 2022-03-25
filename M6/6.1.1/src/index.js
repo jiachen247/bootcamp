195606c77658f4930d3e8e236ecc5c2450954610
jiachen247 <jiachen247@gmail.com>
2022-03-25 12:39:13 +0800

    ADd webpack demo

---
+++ M6/6.1.1/src/index.js
@@ -0,0 +1,7 @@
+// index.js
+import { cat } from './cat.js';
+import { mouse } from './mouse.js';
+
+console.log('This is from index.js');
+console.log('This is imported from cat.js', cat);
+console.log('This is imported from mouse.js', mouse);
---
+++ M6/6.1.2/src/index.js
@@ -0,0 +1,7 @@
+import { cat } from './cat.js';
+import { mouse } from './mouse.js'; 
+import catNames from 'cat-names';
+
+console.log(catNames.random());
+console.log('cat', cat);
+console.log('mouse', mouse);
