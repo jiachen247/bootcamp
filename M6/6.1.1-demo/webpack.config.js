eea1c9dfdac96045334833e711aba64995d704ba
jiachen247 <jiachen247@gmail.com>
2022-03-25 12:38:25 +0800

    ADd webpack demo

---
+++ M6/6.1.1-demo/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
