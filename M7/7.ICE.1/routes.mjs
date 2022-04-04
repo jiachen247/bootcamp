cddd0bcba00ffac589e8e644ec974deed959da07
raivatshah <hiraivat@gmail.com>
2022-04-04 15:50:55 +0530

    add started code for 7.ICE.1 and setup webpack

---
+++ M7/7.ICE.1/routes.mjs
@@ -0,0 +1,9 @@
+import { resolve } from 'path';
+import db from './models/index.mjs';
+
+export default function bindRoutes(app) {
+  // special JS page. Include the webpack index.html file
+  app.get('/home', (request, response) => {
+    response.sendFile(resolve('dist', 'main.html'));
+  });
+}
