cc1ffee691aee24bfc9691f8df60d8c95e10ecae
raivatshah <hiraivat@gmail.com>
2022-04-08 11:49:38 +0530

    complete setup including webpack

---
+++ M7/7.ICE.2/webpack_conf/webpack.common.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name]-[contenthash].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    // Replace previously-compiled files with latest one on each build
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // Name this file main so it does not get automatically requested as a static file
+      filename: 'main.html',
+      template: path.resolve(__dirname, '..', 'src', 'main.html')
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        // Regex to decide which files to run Babel on
+        test: /\.(js|mjs|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
