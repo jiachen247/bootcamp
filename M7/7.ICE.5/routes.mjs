bd92f55c24eab3243b68d58b9513926560acd67d
raivatshah <hiraivat@gmail.com>
2022-04-17 14:06:12 +0530

    7.ICE.5: add item create controller on server side

--- M7/7.ICE.5/routes.mjs
+++ M7/7.ICE.5/routes.mjs
@@ -6,6 +6,7 @@ import initItemsController from './controllers/items.mjs'
 export default function routes (app) {
   const ItemsController = initItemsController(db)
   app.get('/items', ItemsController.index)
+  app.post('/items', ItemsController.create)
 
   // special JS page. Include the webpack index.html file
   app.get('/', (request, response) => {
